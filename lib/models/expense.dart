8f425ed59824514b292d04b8d6372762b6c3b164
subhash <ratusubhash2002@gamil.com>
2026-05-07 20:22:13 +0545

    dgd

---
+++ lib/models/expense.dart
@@ -0,0 +1,15 @@
+class Expense {
+  String title;
+  double amount;
+  String category;
+  DateTime date;
+
+  Expense({
+
+  required this.title,
+  required this.amount,
+  required this.category,
+  required this.date,
+  });
+
+}
